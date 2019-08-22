ad6a45ae54c979ee9bcb30a4eb74d6aebb83972b
Máté Aller <Mate.Aller@mrc-cbu.cam.ac.uk>
2019-08-22 15:24:31 +0100

    Update subject spec

--- BCI_generateSubjectSpec.m
+++ BCI_generateSubjectSpec.m
@@ -10,6 +10,33 @@ update = p.Results.update;
 
 s = subjSpec();
 %%
+subID = 'meg19_0233';
+if ~s.subjPresent(subID) || update
+    % Subject ID
+    if ~update, s.addSubj(subID); end
+    % MEG files to be included
+    fileName = {...
+        'run1'
+        'run2'
+        'run3'
+        'run4'
+        'run5'};
+    % The number of sessions started in the files
+    nRunsInFile = ones(size(fileName));
+    % The serial number within the files of the runn(s) to be excluded
+    exclude = num2cell(NaN(size(fileName)));
+    % The overall running index of not excluded runs within the files
+    % accounting for the behavioural only first day as well. 
+    iRunOverall = num2cell(1:numel(fileName))';
+    s.addField(subID,'meg_files',table(fileName,nRunsInFile,exclude,iRunOverall));
+    % Pre-processing parameters that can change individually
+    s.addField(subID,'preproc_param',...
+        orderfields(struct('cutoff_zval',10,'hp_freq',0.1)));
+    % Notes
+    s.addField(subID,'notes',sprintf(''));
+end
+
+%%
 subID = 'meg19_0239';
 if ~s.subjPresent(subID) || update
     % Subject ID
