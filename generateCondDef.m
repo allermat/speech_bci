06fd0c935c8a3c644008d3056181c971a9ccfee9
Máté Aller <Mate.Aller@mrc-cbu.cam.ac.uk>
2019-11-22 14:25:53 +0000

    Bug fix: corrected condition-trigger assignment

--- generateCondDef.m
+++ generateCondDef.m
@@ -1,7 +1,8 @@
 function condDef = generateCondDef()
 
 % Words used in the experiment
-wordId = {'yes','no','maybe',''}';
+% wordId = {'yes','no','maybe',''}';
+wordId = cat(2,sort({'yes','no','maybe'}),{''})';
 condition = (1:numel(wordId))';
 stimType = cat(1,repmat({'word'},3,1),'noise');
 
