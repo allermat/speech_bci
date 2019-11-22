7fd4e9da33cb32e8882fb2a3ee295fce648ffd7e
Máté Aller <Mate.Aller@mrc-cbu.cam.ac.uk>
2019-11-22 14:24:11 +0000

    Reverted presentation scripts to 474e761, before pilot 2 stim development

--- presentation/BCI_generateAllStimuli.m
+++ presentation/BCI_generateAllStimuli.m
@@ -1,10 +1,6 @@
-function [stimAll,stimKeyAll,stimDurAll,targetWordsAll,nTargetsAll] = BCI_generateAllStimuli(subjectId,varargin)
+function [stimAll,stimKeyAll,targetWordsAll,nTargetsAll] = BCI_generateAllStimuli(subjectId,varargin)
 
-validNoiseWordSelection = {'target','rand10','rand50','rand100'};
-validStimulusTypes = {'pcentred_matt_2hz','pcentred_matt',...
-                      'pcentred_benedikt','non-pcentred_pilot_2_1',...
-                      'non-pcentred_pilot_2_2'};
-validVocodeMethods = {'VOCODER','STRAIGHT'};
+validNoiseModes = {'allTokens','twoTokens','oneTokenRand'};
 
 p = inputParser;
 
@@ -14,32 +10,21 @@ addParameter(p,'nRuns',6,@(x) validateattributes(x,{'numeric'}, ...
              {'scalar','integer','nonnegative'}));
 addParameter(p,'nTrialsPerRun',12,@(x) validateattributes(x,{'numeric'}, ...
              {'scalar','integer','nonnegative'}));
-addParameter(p,'stimulusType','non-pcentred_pilot_2_2',@(x) ismember(x,validStimulusTypes));
-addParameter(p,'jitter',0.2,@(x) validateattributes(x,{'numeric'}, ...
+addParameter(p,'wordFreq',1.6,@(x) validateattributes(x,{'numeric'}, ...
              {'scalar','nonnegative'}));
-addParameter(p,'prestim',0.1,@(x) validateattributes(x,{'numeric'}, ...
-             {'scalar','nonnegative'}));
-addParameter(p,'noiseWordSelection','target',@(x) ismember(x,validNoiseWordSelection));
-addParameter(p,'noiseLpCutoff',30,@(x) validateattributes(x,{'numeric'}, ...
-             {'scalar','integer','nonnegative'}));
+addParameter(p,'noiseMode','oneTokenRand',@(x) ismember(x,validNoiseModes));
 addParameter(p,'randSeed',[],@(x) validateattributes(x,{'numeric'}, ...
              {'scalar','integer','positive'}));
-addParameter(p,'vocodeMethod','STRAIGHT',@(x) ismember(x,validVocodeMethods));
 addParameter(p,'saveFile',true,@(x) validateattributes(x,{'logical'}, ...
              {'scalar'}));
-
 parse(p,subjectId,varargin{:});
 
 subjectId = p.Results.subjectId;
 nRuns = p.Results.nRuns;
 nTrialsPerRun = p.Results.nTrialsPerRun;
-stimulusType = p.Results.stimulusType;
-jitter = p.Results.jitter;
-prestim = p.Results.prestim;
-noiseWordSelection = p.Results.noiseWordSelection;
-noiseLpCutoff = p.Results.noiseLpCutoff;
+wordFreq = p.Results.wordFreq;
+noiseMode = p.Results.noiseMode;
 randSeed = p.Results.randSeed;
-vocodeMethod = p.Results.vocodeMethod;
 saveFile = p.Results.saveFile;
 
 if ismember('randSeed',p.UsingDefaults)
@@ -47,34 +32,20 @@ if ismember('randSeed',p.UsingDefaults)
 else
     rng(randSeed);
 end
-
 % Getting file names
-switch stimulusType
-    case 'pcentred_matt_2hz'
-        inputDir = fullfile(BCI_setupdir('stimuli'),stimulusType);
-        filesNoise = dir(fullfile(inputDir,'*_2Hz.wav'));
-        filesNoise = {filesNoise.name}';
+inputDir = BCI_setupdir('stimuli');
+saveDf = cd(inputDir);
+switch wordFreq
+    case 2
+        filesNoise = dir('*_2Hz.wav');
         filesWords = {'yes_yes-maybe_2Hz.wav'
                       'yes_yes-thirsty_2Hz.wav'
                       'no_no-maybe_2Hz.wav'
                       'no_no-thirsty_2Hz.wav'
                       'maybe_maybe-thirsty_2Hz.wav'
                       'maybe_yes-maybe_2Hz.wav'};
-        % Removing 'thirsty' from the set of words
-        filesNoise = filesNoise(~cellfun(@isempty,...
-            regexp(filesNoise,'^(yes|no|maybe).*\.wav')));
-        wordsLoaded = regexp(filesWords,'(\w)*_.*','tokens','once');
-        wordsLoaded = [wordsLoaded{:}];
-        uniqueWords = unique(wordsLoaded);
-        nUniqueWords = numel(uniqueWords);
-        % To match the keys of the previous pilot yes-1, no-2, maybe-3
-        wordKey = [3,2,1];
-        nRepetitionPerWord = 12; % number of repetitions per words
-        nNoiseStimuli = nRepetitionPerWord*nUniqueWords; % number of noise stimuli per trial
-        nRepetitionMinimum = 10;
-    case 'pcentred_matt'
-        inputDir = fullfile(BCI_setupdir('stimuli'),stimulusType);
-        filesNoise = dir(inputDir);
+    case 1.6
+        filesNoise = dir;
         filesNoise = {filesNoise.name}';
         filesNoise = filesNoise(~cellfun(@isempty,... 
             regexp(filesNoise,'[a-zA-Z]*_[a-zA-Z]*-[a-zA-Z]*.wav')));
@@ -84,188 +55,64 @@ switch stimulusType
                       'no_no-thirsty.wav'
                       'maybe_maybe-thirsty.wav'
                       'maybe_yes-maybe.wav'};
-        % Removing 'thirsty' from the set of words
-        filesNoise = filesNoise(~cellfun(@isempty,...
-            regexp(filesNoise,'^(yes|no|maybe).*\.wav')));
-        wordsLoaded = regexp(filesWords,'(\w)*_.*','tokens','once');
-        wordsLoaded = [wordsLoaded{:}];
-        uniqueWords = unique(wordsLoaded);
-        nUniqueWords = numel(uniqueWords);
-        % To match the keys of the previous pilot yes-1, no-2, maybe-3
-        wordKey = [3,2,1];
-        nRepetitionPerWord = 12; % number of repetitions per words
-        nNoiseStimuli = nRepetitionPerWord*nUniqueWords; % number of noise stimuli per trial
-        nRepetitionMinimum = 10;
-    case 'pcentred_benedikt'
-        inputDir = fullfile(BCI_setupdir('stimuli'),stimulusType);
-        filesWords = {'bread.wav'
-                      'else.wav'
-                      'fine.wav'
-                      'like.wav'
-                      'pair.wav'
-                      'thin.wav'};
-        switch noiseWordSelection
-            case {'rand10','rand50','rand100'}
-                filesNoise = dir(fullfile(inputDir,'*.wav'));
-                filesNoise = {filesNoise.name}';
-                idx = randperm(numel(filesNoise));
-                n = regexp(noiseWordSelection,'[a-z]*([0-9]*)','tokens','once');
-                n = str2double(n{:});
-                filesNoise = filesNoise(idx(1:n));
-            case 'target'
-                filesNoise = filesWords;
-        end
-        wordsLoaded = regexp(filesWords,'(\w)*.wav','tokens','once');
-        wordsLoaded = [wordsLoaded{:}]';
-        uniqueWords = unique(wordsLoaded);
-        nUniqueWords = numel(uniqueWords);
-        wordKey = 1:nUniqueWords;
-        nRepetitionPerWord = 8; % number of repetitions per words
-        nNoiseStimuli = 16; % number of noise stimuli per trial
-        nRepetitionMinimum = 8;
-    case 'non-pcentred_pilot_2_1'
-        inputDir = fullfile(BCI_setupdir('stimuli'),'non-pcentred_pilot_2','uniform');
-        filesWords = {'yes.wav'
-                      'no.wav'
-                      'help.wav'
-                      'pain.wav'
-                      'left.wav'
-                      'right.wav'};
-        switch noiseWordSelection
-            case {'rand10','rand50','rand100'}
-                error('This option is not implemented! ');
-            case 'target'
-                filesNoise = filesWords;
-        end
-        wordsLoaded = regexp(filesWords,'(\w)*.wav','tokens','once');
-        wordsLoaded = [wordsLoaded{:}]';
-        uniqueWords = unique(wordsLoaded);
-        nUniqueWords = numel(uniqueWords);
-        wordKey = 1:nUniqueWords;
-        nRepetitionPerWord = 8; % number of repetitions per words
-        nNoiseStimuli = 16; % number of noise stimuli per trial
-        nRepetitionMinimum = 8;
-    case 'non-pcentred_pilot_2_2'
-        inputDir = fullfile(BCI_setupdir('stimuli'),'non-pcentred_pilot_2','uniform');
-        filesWords = {'yes.wav'
-                      'no.wav'
-                      'help.wav'
-                      'pain.wav'
-                      'hot.wav'
-                      'cold.wav'};
-        switch noiseWordSelection
-            case {'rand10','rand50','rand100'}
-                error('This option is not implemented! ');
-            case 'target'
-                filesNoise = filesWords;
-        end
-        wordsLoaded = regexp(filesWords,'(\w)*.wav','tokens','once');
-        wordsLoaded = [wordsLoaded{:}]';
-        uniqueWords = unique(wordsLoaded);
-        nUniqueWords = numel(uniqueWords);
-        wordKey = 1:nUniqueWords;
-        nRepetitionPerWord = 8; % number of repetitions per words
-        nNoiseStimuli = 16; % number of noise stimuli per trial
-        nRepetitionMinimum = 8;
     otherwise
-        error('Unrecognized word stimulus type');
+        error('Unrecognized word frequency');
 end
+cd(saveDf);
+
+% Removing 'thirsty' from the set of words
+filesNoise = filesNoise(~cellfun(@isempty,...
+                        regexp(filesNoise,'^(yes|no|maybe).*\.wav')));
 
 % First read the words from disk and perform the vocoding on the already
 % loaded data, it is more economical this way.
-% NOISE words
-[y,fs] = cellfun(@audioread,fullfile(inputDir,filesNoise'),...
+[y,Fs] = cellfun(@audioread,fullfile(inputDir,filesNoise'),...
                  'UniformOutput',false);
-audioNoise = cell2struct(cat(1,y,fs),{'y','Fs'},1);
+audioNoise = cell2struct(cat(1,y,Fs),{'y','Fs'},1);
 
-% WORDS
-[y,fs] = cellfun(@audioread,fullfile(inputDir,filesWords'),...
-                 'UniformOutput',false);
-audioWords = cell2struct(cat(1,y,fs),{'y','Fs'},1);
-
-if regexp(stimulusType,'^pcentred','once')
-    % Making sure all files are the same length. Sometimes they differ by 1
-    % sample.
-    minLength = min(cellfun(@length,{audioNoise.y}));
-    for i = 1:size(audioNoise,1)
-        audioNoise(i).y = audioNoise(i).y(1:minLength);
-    end
-    
-    
-    % Making sure all files are the same length. Sometimes they differ by 1
-    % sample.
-    minLength = min(cellfun(@length,{audioWords.y}));
-    for i = 1:size(audioWords,1)
-        audioWords(i).y = audioWords(i).y(1:minLength);
-    end
-    
-else
-%     % Padding files to the same length
-%     padTo = 0.63; % Pad to this length in s
-%     for i = 1:size(audioNoise,1)
-%         audioNoise(i).y = cat(1,audioNoise(i).y,...
-%             zeros(round(padTo*audioNoise(i).Fs)-size(audioNoise(i).y,1),1));
-%     end
-%     
-%     for i = 1:size(audioWords,1)
-%         audioWords(i).y = cat(1,audioWords(i).y,...
-%             zeros(round(padTo*audioWords(i).Fs)-size(audioWords(i).y,1),1));
-%     end
-end
-
-% Making sure that the length of all files in milliseconds is a whole
-% number (STRAIGHT seems to crash if this is not the case)
+% Making sure all files are the same length. Sometimes they differ by 1
+% sample. 
+minLength = min(cellfun(@length,{audioNoise.y}));
 for i = 1:size(audioNoise,1)
-    endIdx = find(mod((1:numel(audioNoise(i).y))/audioNoise(i).Fs*1000,1) == 0,...
-        1,'last');
-    audioNoise(i).y(endIdx+1:end) = [];
+    audioNoise(i).y = audioNoise(i).y(1:minLength);
 end
-
-% Making sure that the length of all files in milliseconds is a whole
-% number (STRAIGHT seems to crash if this is not the case)
-for i = 1:size(audioWords,1)
-    endIdx = find(mod((1:numel(audioWords(i).y))/audioWords(i).Fs*1000,1) == 0,...
-        1,'last');
-    audioWords(i).y(endIdx+1:end) = [];
-end
-
 % match RMS of input files
 for f=1:size(audioNoise,1)
-    audioNoise(f).y = audioNoise(f).y .* 1./rms(audioNoise(f).y);
+   audioNoise(f).y = audioNoise(f).y .* 1./rms(audioNoise(f).y);
 end
 
-% match RMS of input files
-for f=1:size(audioWords,1)
-    audioWords(f).y = audioWords(f).y .* 1./rms(audioWords(f).y);
-end
-
-S.audioWords = audioWords;
-switch noiseWordSelection
-    case {'rand10','rand50','rand100'}
+wordsLoaded = regexp(filesWords,'(\w)*_.*','tokens','once');
+wordsLoaded = [wordsLoaded{:}];
+uniqueWords = unique(wordsLoaded);
+nUniqueWords = numel(uniqueWords);
+% To match the keys of the previous pilot yes-1, no-2, maybe-3
+wordKey = [3,2,1];
+S.audioWords = audioNoise(ismember(filesNoise,filesWords));
+switch noiseMode
+    case 'allTokens'
         S.audioNoise = audioNoise;
         S.randSelect = false;
-    case 'target'
+    case 'twoTokens'
         S.audioNoise = S.audioWords;
         S.randSelect = false;
+    case 'oneTokenRand'
+        S.audioNoise = S.audioWords;
+        S.randSelect = true;
 end
-S.fs = fs;
 S.wordsLoaded = wordsLoaded;
-S.jitter = jitter;
-S.prestim = prestim;
 S.nCh = 16; % number of channels for vocoding
-S.nRepetitionPerWord = nRepetitionPerWord; % number of repetitions per words
-S.nNoiseStimuli = nNoiseStimuli; % number of noise stimuli per trial
-S.nRepetitionMinimum = nRepetitionMinimum;
+S.nRepetitionPerWord = 12; % number of repetitions per words
+S.nNoiseStimuli = S.nRepetitionPerWord*nUniqueWords; % number of noise stimuli per trial
+S.nRepetitionMinimum = 10;
 S.wordKey = wordKey;
-S.vocodeMethod = vocodeMethod;
-S.noiseLpCutoff = noiseLpCutoff; % LP filter cutoff frequency (Hz) for noise vocoding
+S.noiseLpCutoff = 5; % LP filter cutoff frequency (Hz) for noise vocoding
 
 % Defining target words and how many times they are presented
 targetWords = uniqueWords;
 if nTrialsPerRun == 1
     % This option is only relevant for devMode
-    targetWordsAll = targetWords(randi(nUniqueWords));
-    nTargetsAll = nRepetitionPerWord;
+    targetWordsAll = targetWords(randi(3));
+    nTargetsAll = 12;
 elseif mod(nTrialsPerRun/nUniqueWords,1) ~= 0
     error(['The number of trials per run must be a multiple of the number ',...
            'of target words']);
@@ -275,12 +122,9 @@ else
     targetWordsAll = repmat(targetWordsAll(:)',nRuns,1);
     % Number of target words. This makes sure that across the whole
     % experiment each target word will be presented the same amount of 
-    % times.
-    temp = [];
-    for i = 1:(nTrialsPerRun*nRuns/3)
-        temp = cat(1,temp,circshift([8,9,10]',i-1));
-    end
-    nTargetsAll = reshape(temp,nTrialsPerRun,[])';
+    % times. 
+    nTargetsAll = repmat([10,11,12;12,10,11;11,12,10],1,nTrialsPerRun/3);
+    nTargetsAll = repmat(nTargetsAll,nRuns/3,1);
     for i = 1:nRuns
         randVec = randperm(nTrialsPerRun);
         targetWordsAll(i,:) = targetWordsAll(i,randVec);
@@ -296,12 +140,12 @@ if numel(unique(check)) > 1
 end
 
 % Generating stimului
-[stimAll,stimKeyAll,stimDurAll] = deal(cell(nRuns,nTrialsPerRun));
+[stimAll,stimKeyAll] = deal(cell(nRuns,nTrialsPerRun));
 for i = 1:nRuns
     for j = 1:nTrialsPerRun
         S.targetKey = wordKey(ismember(targetWords,targetWordsAll(i,j)));
         S.nTargets = nTargetsAll(i,j);
-        [stimAll{i,j},stimKeyAll{i,j},stimDurAll{i,j}] = BCI_generateStimulus(S);
+        [stimAll{i,j},stimKeyAll{i,j}] = BCI_generateStimulus(S);
     end
 end
 
@@ -311,7 +155,7 @@ if saveFile
     if exist(filePath,'file')
         delete(filePath);
     end
-    save(filePath,'stimAll','stimKeyAll','stimDurAll','targetWordsAll','nTargetsAll');
+    save(filePath,'stimAll','stimKeyAll','targetWordsAll','nTargetsAll');
 end
 
 end
--- presentation/BCI_generateStimulus.m
+++ presentation/BCI_generateStimulus.m
@@ -1,11 +1,10 @@
-function [stim,stimKey,stimDuration] = BCI_generateStimulus(S)
+function [stim,stimKey] = BCI_generateStimulus(S)
 
 p = inputParser;
 
-fieldsReqS = {'audioNoise','audioWords','wordsLoaded','jitter','prestim','nCh',...
-              'nNoiseStimuli','nRepetitionPerWord','nTargets',...
-              'noiseLpCutoff','targetKey','nRepetitionMinimum','wordKey',...
-              'randSelect'};
+fieldsReqS = {'audioNoise','audioWords','wordsLoaded','nCh','nNoiseStimuli',...
+              'nRepetitionPerWord','nTargets','noiseLpCutoff','targetKey',...
+              'nRepetitionMinimum','wordKey','randSelect'};
 
 checkS = @(x) all(isfield(x,fieldsReqS));
 
@@ -19,29 +18,15 @@ S = p.Results.S;
 audioNoise = S.audioNoise;
 audioWords = S.audioWords;
 wordsLoaded = S.wordsLoaded;
-vocodeMethod = S.vocodeMethod;
-jitter = S.jitter;
-prestim = S.prestim;
 nCh = S.nCh;
 nNoiseStimuli = S.nNoiseStimuli;
 nRepetitionPerWord = S.nRepetitionPerWord;
-% nRepetitionMinimum = S.nRepetitionMinimum;
+nRepetitionMinimum = S.nRepetitionMinimum;
 wordKey = S.wordKey;
 uniqueWords = unique(wordsLoaded);
+nUniqueWords = numel(uniqueWords);
 randSelect = S.randSelect;
 noiseLpCutoff = S.noiseLpCutoff;
-
-% Applying STRAIGHT only once for each token
-if strcmp(vocodeMethod,'STRAIGHT')
-    if ~randSelect
-        noise_straight = BCI_generateNoiseStraight(audioNoise, ...
-                    'lpCutoff',noiseLpCutoff);
-    end
-    words_straight = cell(size(audioWords));
-    for i = 1:numel(words_straight)
-        words_straight{i} = applyStraight(audioWords(i));
-    end
-end
 
 % Generate noise
 noise = cell(nNoiseStimuli,1);
@@ -58,98 +43,60 @@ for i = 1:nNoiseStimuli
             'UniformOutput',false);
         % Choose randomly either of the instances of each word
         idx = cellfun(@(x) x(randi(numel(x))),temp);
-        switch vocodeMethod
-            case 'VOCODER'
-                noise{i} = BCI_generateVocodedNoise(audioNoise(idx),nCh, ...
-                    'lpCutoff',noiseLpCutoff);
-            case 'STRAIGHT'
-                noise{i} = BCI_generateNoiseStraight(audioNoise(idx), ...
-                    'lpCutoff',noiseLpCutoff);
-        end
+        noise{i} = BCI_generateVocodedNoise(audioNoise(idx),nCh, ...
+            'lpCutoff',noiseLpCutoff);
     else
         % Use all available tokens to generate noise
-        switch vocodeMethod
-            case 'VOCODER'
-                noise{i} = BCI_generateVocodedNoise(audioNoise,nCh, ...
-                    'lpCutoff',noiseLpCutoff);
-            case 'STRAIGHT'
-                noise{i} = noise_straight;
-        end
-    end
-    % Add jitter if necessary
-    if jitter > 0
-        noise{i} = cat(2,noise{i},zeros(1,round((prestim+jitter*rand())*audioNoise(1).Fs)));
+        noise{i} = BCI_generateVocodedNoise(audioNoise,nCh, ...
+            'lpCutoff',noiseLpCutoff);
     end
 end
 
 % Vocode words
-wordKeysAll = repmat(wordKey,nRepetitionPerWord,1);
-wordKeysAll = wordKeysAll(:);
-% Adding instances of target word if necessary
-if S.nTargets > nRepetitionPerWord
-    nToAdd = nRepetitionPerWord-S.nTargets;
-    wordKeysAll = cat(1,wordKeysAll,repmat(S.targetKey,nToAdd,1));
-end
-wordsAll = cell(size(wordKeysAll));
-
-for iWord = 1:numel(wordsAll)
-    idx = find(ismember(wordsLoaded,uniqueWords{wordKey == wordKeysAll(iWord)}));
-%     idx = idx(randi(numel(idx)));
-    switch vocodeMethod
-        case 'VOCODER'
-            % Choose randomly either of the instances of the words
-            [~,~,~,wordsAll{iWord}] = vocode_ma('noise','n','greenwood','half', ...
-                30,nCh,audioWords(idx),'');
-            
-        case 'STRAIGHT'
-            wordsAll{iWord} = words_straight{idx};
-    end
-    % Add jitter if necessary
-    if jitter > 0
-        wordsAll{iWord} = cat(2,wordsAll{iWord},...
-            zeros(1,round((prestim+jitter*rand())*audioNoise(1).Fs)));
+wordsAll = cell(nRepetitionPerWord,nUniqueWords);
+wordKeysAll = NaN(nRepetitionPerWord,nUniqueWords);
+for iRep = 1:nRepetitionPerWord
+    for iWord = 1:nUniqueWords
+        idx = find(ismember(wordsLoaded,uniqueWords{iWord}));
+        % Choose randomly either of the instances of the words
+        [~,~,~,wordsAll{iRep,iWord}] = vocode_ma('noise','n','greenwood','half', ...
+            30,nCh,audioWords(idx(randi(2))),'');
+        wordKeysAll(iRep,iWord) = wordKey(iWord);
     end
 end
-
 noiseKey = numel(wordKey)+1;
 
 % Devide targ into two subparts and randomize separately
-% stim_1 = wordsAll(1:nRepetitionMinimum,:);
-% stim_1 = stim_1(:);
-% stim_1 = cat(1,stim_1,cat(1,noise(1:nRepetitionMinimum*nUniqueWords)));
-% stimKey_1 = wordKeysAll(1:nRepetitionMinimum,:);
-% stimKey_1 = stimKey_1(:);
-% stimKey_1 = cat(1,stimKey_1,cat(1,noiseKey*ones(nRepetitionMinimum*nUniqueWords,1)));
-% randVector = randperm(size(stim_1,1));
-% stim_1 = stim_1(randVector);
-% stimKey_1 = stimKey_1(randVector);
-% 
-% stim_2 = wordsAll(nRepetitionMinimum+1:end,:);
-% stim_2 = stim_2(:);
-% stim_2 = cat(1,stim_2,cat(1,noise(nRepetitionMinimum*nUniqueWords+1:end)));
-% stimKey_2 = wordKeysAll(nRepetitionMinimum+1:end,:);
-% stimKey_2 = stimKey_2(:);
-% stimKey_2 = cat(1,stimKey_2,...
-%                 cat(1,noiseKey*ones(nNoiseStimuli-nRepetitionMinimum*nUniqueWords,1)));
-% randVector = randperm(size(stim_2,1));
-% stim_2 = stim_2(randVector);
-% stimKey_2 = stimKey_2(randVector);
-% % Removing instances of target word if necessary
-% if S.nTargets < S.nRepetitionPerWord
-%     nToRemove = S.nRepetitionPerWord-S.nTargets;
-%     idx = find(ismember(stimKey_2,S.targetKey));
-%     stim_2(idx(1:nToRemove)) = [];
-%     stimKey_2(idx(1:nToRemove)) = [];
-% end
+stim_1 = wordsAll(1:nRepetitionMinimum,:);
+stim_1 = stim_1(:);
+stim_1 = cat(1,stim_1,cat(1,noise(1:nRepetitionMinimum*nUniqueWords)));
+stimKey_1 = wordKeysAll(1:nRepetitionMinimum,:);
+stimKey_1 = stimKey_1(:);
+stimKey_1 = cat(1,stimKey_1,cat(1,noiseKey*ones(nRepetitionMinimum*nUniqueWords,1)));
+randVector = randperm(size(stim_1,1));
+stim_1 = stim_1(randVector);
+stimKey_1 = stimKey_1(randVector);
 
-stim = cat(1,wordsAll,noise);
-stimKey = cat(1,wordKeysAll,noiseKey*ones(size(noise)));
-randVector = pseudorandomize_stimuli(stimKey);
-stim = stim(randVector);
-stimKey = stimKey(randVector);
-stimDuration = cellfun(@(x) numel(x)/audioNoise(1).Fs,stim);
-stim = cat(2,stim{:});
+stim_2 = wordsAll(nRepetitionMinimum+1:end,:);
+stim_2 = stim_2(:);
+stim_2 = cat(1,stim_2,cat(1,noise(nRepetitionMinimum*nUniqueWords+1:end)));
+stimKey_2 = wordKeysAll(nRepetitionMinimum+1:end,:);
+stimKey_2 = stimKey_2(:);
+stimKey_2 = cat(1,stimKey_2,...
+                cat(1,noiseKey*ones(nNoiseStimuli-nRepetitionMinimum*nUniqueWords,1)));
+randVector = randperm(size(stim_2,1));
+stim_2 = stim_2(randVector);
+stimKey_2 = stimKey_2(randVector);
+% Removing instances of target word if necessary
+if S.nTargets < S.nRepetitionPerWord
+    nToRemove = S.nRepetitionPerWord-S.nTargets;
+    idx = find(ismember(stimKey_2,S.targetKey));
+    stim_2(idx(1:nToRemove)) = [];
+    stimKey_2(idx(1:nToRemove)) = [];
+end
+stim = cat(2,stim_1{:},stim_2{:});
 % Normalize stimulus between +/-1
 stim = stim./max(abs(stim));
+stimKey = cat(2,stimKey_1',stimKey_2');
 
 end
